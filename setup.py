8602d2ff388277e47627d6c129df9de56242b44b
abelyakov <abelyakov@GDSpb1260.local>
2018-11-15 18:53:23 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='classification_models',
+    version='',
+    packages=['classification_models', 'classification_models.resnet', 'classification_models.resnext'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
